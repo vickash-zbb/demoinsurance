71b3af15ffa1a7a03509fabfdb1ad35ce0e5257b
vickash-zbb <vickashshivanzbb@gmail.com>
2024-03-13 15:53:27 +0530

    add all

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// our story
+document.addEventListener =
+  ("DOMContentLoaded",
+  function () {
+    let OruStory = document.querySelector(".our_story");
+    let storyList = document.querySelector(".story_list");
+    let menuTab = OruStory.querySelectorAll(".story_list > div");
+    let storyBody = OruStory.querySelector(".story_body");
+    let storyBodyElement = OruStory.querySelectorAll(".story_body > div");
+
+    menuTab.forEach((tab, index) => {
+      tab.addEventListener("click", function () {
+        storyList.querySelector(".active").classList.remove("active");
+        tab.classList.add("active");
+
+        storyBody.querySelector(".active").classList.remove("active");
+        storyBodyElement[index].classList.add("active");
+      });
+    });
+  });
+
+// our story end
+
+// accordion start
+const accordion = document.getElementsByClassName("accordion-list-container");
+for (let index = 0; index < accordion.length; index++) {
+  accordion[index].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
