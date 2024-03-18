b38c7e469a0fc0203e2deb50d85df1a7a90ea9e5
vickash-zbb <vickashshivanzbb@gmail.com>
2024-03-18 17:29:41 +0530

    index complete

--- js/script.js
+++ js/script.js
@@ -35,3 +35,41 @@ const menulist = document.querySelector("nav .nav-bar");
 menu.addEventListener("click", function () {
   menulist.classList.toggle("showmenu");
 });
+// nav scrol function
+function nav(scroll) {}
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+var stickyOffset = $(".nav-bar_nav").offset().top;
+
+$(window).scroll(function () {
+  var nav = $(".nav-bar_nav"),
+    scroll = $(window).scrollTop();
+
+  if (scroll >= stickyOffset) nav.addClass("sticky");
+  else nav.removeClass("sticky");
+});
+
+$(window).scroll(function () {
+  var sticky = $(".nav-bar_nav"),
+    scroll = $(window).scrollTop();
+
+  if (scroll >= 100) sticky.addClass("fixed");
+  else sticky.removeClass("fixed");
+});
